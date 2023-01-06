5247d0f5bd128b18977a4ca332597ca73b28ed7f
MateoTarlev <101196557+MateoTarlev@users.noreply.github.com>
2023-01-06 23:54:36 +0200

    send_receiv

--- receiver.py
+++ receiver.py
@@ -1,12 +1,12 @@
 import socket
-from random import random
-import threading
 import time
-
+from random import random
+import tkinter as tk
+import threading
 class Reciver:
-    def __init__(self, ip, port):
-        self.UDP_IP = ip
-        self.UDP_PORT = port
+    def __init__(self):
+        self.UDP_IP = ""
+        self.UDP_PORT = ""
         self.receptie=[]
 
     def decodeData(self,packet):
@@ -19,55 +19,91 @@ class Reciver:
         packets = packet[1:5]
         fname = packet[5:]
         return packets, fname
+    def validateLogin(self):
+        self.UDP_IP=self.ip.get()
+        self.UDP_PORT=self.port.get()
+        self.ok=2
+    def interface(self):
+
+        self.root=tk.Tk()
+        self.root.geometry("920x720")
+        self.root.title("SERVER")
+
+        self.root.iconphoto(True, tk.PhotoImage(file ='virtual-server-icon-7.png') )
+        self.root.config(background="#dfede9")
+
+        self.T = tk.Text(self.root, height=32, width=82)
+        self.port=tk.StringVar()
+        self.ip=tk.StringVar()
+        self.IPLabel = tk.Label(self.root, text="IP:").grid(row=1, column=0)
+        self.IPEntry = tk.Entry(self.root, textvariable=self.ip).grid(row=1, column=1)
+        self.PORTLabel = tk.Label(self.root, text="PORT").grid(row=2, column=0)
+        self.PORTEntry = tk.Entry(self.root, textvariable=self.port, show='').grid(row=2, column=1)
+        self.CONNECTButton = tk.Button(self.root, text="CONNECT", command=self.validateLogin).grid(row=5, column=1)
+
+        self.T.place(x=200,y=0)
+
+
+        self.root.mainloop()
 
     def start(self):
+        threading.Thread(target=self.interface).start()
+        time.sleep(10)
+
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.bind((self.UDP_IP, self.UDP_PORT))
+        self.s.bind((self.UDP_IP, int(self.UDP_PORT)))
         print("witing for connecting")
+        self.mesaj=tk.Label(self.root,text="✔Ai setat cu SUCCES iP si Port").place(x=32,y=70)
+
         pachet_asteptat = 1
         counter = 0;
         while True:
-
+            #self.T.insert(tk.END, "waiting for conecting")
             #for i in range(1, 56):
             packet, address = self.s.recvfrom(1024)
             packet = bytes(packet).decode()
+            self.T.insert(tk.END, "\n\t\t_______________________________________________\n\t\tAm primit de la:"+str(address)+"\n")
             if (packet[0] == "1"):
-                print("am primit un fisier de tip info:")
+                print("         un fisier de tip info:")
                 nrpachete, numefisier = self.decodarePachet(packet)
                 print(f"numarul de pachete:{nrpachete}")
                 print(f"numele fisierului:{numefisier}")
                 self.s.sendto("am primit mesajul".encode('utf-8'), address)
             if (packet[0] == "4"):
-                print("\nam primit un fisier de tip data")
+                print("\nfisier de tip data")
                 id, lungime, continut = self.decodeData(packet)
                 id = int(id)
-
+                self.T.insert(tk.END, "\t\t      un fisier de tip data\n")
                 aux = pachet_asteptat - 1
+                old_pac = pachet_asteptat
                 if( id == aux):
                     pachet_asteptat -=1
 
+                if(old_pac != pachet_asteptat):
+                    self.T.insert(tk.END, "\n\t\t✘TRANSMITEREA anterioara nu a intrat in timp✘\n\t\tPachet asteptat: " + str(pachet_asteptat) + " din nou..\n")
+                else:
+                    self.T.insert(tk.END, "\t\tPachet asteptat: " + str(pachet_asteptat) + "\n")
+
                 print(f"pachet_asteptat {pachet_asteptat} = {id} id???")
                 if id == pachet_asteptat:
-
+                    self.T.insert(tk.END,"\t\tS-a receptionat pachetul: "+str(id)+"\n")
                     print(f"s-a receptionat pachetul{id}")
                     print(continut)
                     a = random()
 
-                    if a > 0.3:
+                    if a > 0.03:
                         self.s.sendto(str(continut).encode('ascii'), address)
                         pachet_asteptat = pachet_asteptat + 1
 
                     else:
                         self.s.sendto(str(404).encode('ascii'), address)
                         #------------
-                        print(f"DAR ERROR LA RETRIMITERE -> pachet_asteptat_{pachet_asteptat}  -=   counter_{counter}")
+                        self.T.insert(tk.END,"\t\t_______________________________________________\n\t\t✘ !EROARE LA RETRIMITEREA pachetului " + str(id) + "! ✘\n\t\t_______________________________________________\n\n")
+                        print(f"EROARE LA RETRIMITERE ")
                         #-----------
-
-
-
-
-
+    def proces(self):
+        threading.Thread(target=self.start).start()
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-reciver = Reciver(UDP_IP, UDP_PORT)
-reciver.start()
+reciver = Reciver()
+reciver.proces()
--- sender.py
+++ sender.py
@@ -1,8 +1,10 @@
 import socket
 import threading
 import datetime
+from tkinter import *
 import time
 from tkinter import filedialog
+import tkinter as tk
 from textwrap import wrap
 
 class Sender:
@@ -13,14 +15,51 @@ class Sender:
         self.continut = []
         self.asamblare = []
         self.pachetID=0
-        self.windowSize=5
+
+
+    def interface(self):
+
+        self.root = tk.Tk()
+        self.root.geometry("920x720")
+        self.root.title("CLIENT  (go back n)")
+
+        #self.root.iconphoto(True, tk.PhotoImage(file='cliente.png'))
+        self.root.config(background="#dfede9")
+
+        self.T = tk.Text(self.root, height=32, width=82)
+        self.timer = tk.StringVar()
+        self.n = tk.StringVar()
+        self.opt = tk.StringVar()
+        self.T.place(x=200, y=0)
+        self.a = tk.Button(self.root, text="Incarcati fisier", command=self.get_fisier).place(x=49, y=23)
+        self.nLabel = tk.Label(self.root, text="   Dimensiunea ferestrei:").place(x=0, y = 100)
+        self.nEntry = tk.Entry(self.root, textvariable=self.n).place(x=9, y = 120)
+        self.timerLabel = tk.Label(self.root, text="Timp transfer(s):").place(x=9, y = 180)
+        self.timerEntry = tk.Entry(self.root, textvariable=self.timer, show='').place(x=9, y = 200)
+
+        self.optLabel = tk.Label(self.root, text="  1 = Info | 2 = Data").place(x=9, y=240)
+        self.optEntry = tk.Entry(self.root, textvariable=self.opt, show='').place(x=9, y=260)
+
+
+        self.root.mainloop()
+
+
+    def get_fisier(self):
+        self.fileName=filedialog.askopenfilename()
+
     def proces(self):
-        fileName = filedialog.askopenfilename()
+        threading.Thread(target=self.interface).start()
+
+        time.sleep(12)
+        fileName = self.fileName
         file = open(fileName, "r+")
         file1 = file.read()
+        self.windowSize = self.n.get()
+        self.timer = self.timer.get()
+
+
 
         packetSize = 10
-
 
         self.continut = wrap(file1, packetSize)
 
@@ -33,12 +72,10 @@ class Sender:
         nrOfPackets = str(nrOfPackets).zfill(4)
         counter = 1
 
-        timp_introdus = 0.062
+        timp_introdus = self.timer
         print(f"numarul de octeti:{fileLength}")
         print(f"Fisierul necesita : {nrOfPackets} de pachete")
-        opt = int(input("Ce doriti sa trimiteti catre server:\n"
-                        "1.Info\n"
-                        "2.Data\n"))
+        opt = int(self.opt.get())
         if (opt == 1):
             tosend = str(self.INFO) + nrOfPackets + fileName;
             tosend1 = bytes(tosend.encode())
@@ -49,44 +86,54 @@ class Sender:
             while self.pachetID <len(self.continut):
                #for i in range(self.windowSize):
                 i = 0
-                while(i < self.windowSize):
+                while(i < int(self.windowSize)):
 
                     tosend = str(self.DATA) + str(counter).zfill(4) + str(nrOfPackets).zfill(4) + str(self.continut[i]);
 
                     start = datetime.datetime.now()
 
                     self.s.sendto(tosend.encode('ascii'), ("127.0.0.1", 5005))
+                    address = 5005
+                    self.T.insert(tk.END,"\n\nAm trimis catre SERVER pachetul:" + str(counter)+"\n")
 
-                    time.sleep(0.05)
+                    time.sleep(0.05) #intarziere intentionata pentru a putea calcula timpul de transfer si primire
 
                     continut1, (addr, port) = self.s.recvfrom(1024)
+
 
                     stop = datetime.datetime.now()
                     delta_timp = stop - start
 
-                    print(f"*****{(delta_timp.total_seconds()  )}*****")
+                    print(f"***{(delta_timp.total_seconds()  )}***")
 
                     continut1 = continut1.decode('utf-8')
 
 
                     delta = float(delta_timp.total_seconds())
                     print(f"DELTA = {delta}   >   timp_introdus{timp_introdus} ? ")
-                    if(timp_introdus < delta):
+                    if(float(timp_introdus) < delta):
                        ok = 1
                     else:
                        ok = 0
+
                     print(f"ok = {ok}")
                     if ((continut1 == "404") or (ok == 1)):
-
+                        self.T.insert(tk.END, "\n_________________________________________________________________________________")
+                        if(continut1 == "404"):
+                            self.T.insert(tk.END,"\n✘ !EROARE PACHETUL  " + str(counter) + " NU A PUTUT AJUNGE! ✘")
+                        if(ok == 1):
+                            self.T.insert(tk.END,"\n✘ Intarziere pachet "+str(counter) + ": (Timp de transfer__" + str(delta) + "  <  " + str(timp_introdus) + "__Timp introdus) ✘")
+                        self.T.insert(tk.END, "\n_________________________________________________________________________________")
                         print(f"----\ncontinut =  {continut1} || id = {self.pachetID}  || counter = {counter} || i = {i}\n")
                     else:
+                        self.T.insert(tk.END, "Pachetul: " + str(counter) + " a ajuns cu succes  ✔\n")
                         print(f"continut =  {continut1} || id = {self.pachetID}  || counter = {counter} || i = {i}")
                         self.asamblare.append(continut1)
                         self.pachetID = self.pachetID + 1
                         counter = counter + 1
                         i = i + 1;
                 del self.continut[0:5]
-                self.pachetID -= self.windowSize
+                self.pachetID -= int(self.windowSize)
 
 
 
